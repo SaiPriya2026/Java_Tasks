78811705799327ea36a3d7ab18ec971a562f8ada
SaiPriya2026 <ssuragani@belenits.in>
2026-06-11 09:14:29 +0000

    Task -1 : Employee Salary Processing

---
+++ src/Employee.java
@@ -0,0 +1,87 @@
+public class Employee
+{
+    private Integer employeeId;
+    private String employeeName;
+    private String department;
+    private String role;
+    private Integer experience;
+    private Double salary;
+    private Double performanceRating;
+
+    public Employee()
+    {
+
+    }
+
+    public Employee(Integer employeeId, String employeeName, String department,String role,Integer experience,Double salary,Double performanceRating)
+    {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.department = department;
+        this.role = role;
+        this.experience = experience;
+        this.salary = salary;
+        this.performanceRating = performanceRating;
+    }
+
+    public Integer getEmployeeId()
+    {
+
+        return employeeId;
+    }
+    public void setEmployeeId(Integer employeeId)
+    {
+
+        this.employeeId = employeeId;
+    }
+    public String getEmployeeName()
+    {
+
+        return employeeName;
+    }
+    public void setEmployeeName(String employeeName)
+    {
+        this.employeeName = employeeName;
+    }
+    public String getDepartment()
+    {
+        return department;
+    }
+    public void setDepartment(String department)
+    {
+        this.department = department;
+    }
+    public String getRole()
+    {
+        return role;
+    }
+    public void setRole(String role)
+    {
+        this.role = role;
+    }
+    public Integer getExperience()
+    {
+        return experience;
+    }
+    public void setExperience(Integer experience)
+    {
+        this.experience = experience;
+    }
+    public Double  getSalary()
+    {
+        return salary;
+    }
+    public void setSalary(Double salary)
+    {
+        this.salary = salary;
+    }
+    public Double  getPerformanceRating()
+    {
+        return performanceRating;
+    }
+    public void setPerformanceRating(Double performanceRating)
+    {
+        this.performanceRating = performanceRating;
+    }
+
+}
